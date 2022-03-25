3b015793582c278a2573e6561a7596fc23329add
Gabor Meszaros <jegkocka88@gmail.com>
2022-03-25 10:16:33 +0100

    Rate-limiting and credit consumption added with respective error MSGs.

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from collections import defaultdict
+from datetime import datetime
 from random import choices
 
 import requests
@@ -22,7 +24,7 @@ class APIKeyRequest(BaseModel):
 class User(BaseModel):
     user_name: str
     api_key: str
-    credits: Optional[int] = 100
+    credits: Optional[int] = 3
 
 
 def generate_random_api_key():
@@ -31,8 +33,15 @@ def generate_random_api_key():
 
 app = FastAPI()
 
+REQUEST_CREDIT_COST = 1
+
 USER_COLLECTION = []
 VALID_APIS = set()
+API_KEY_TO_USER = {}
+
+API_KEY_TO_LAST_API_CALL = defaultdict(lambda: datetime.fromtimestamp(1))
+COOLDOWN_AFTER_LAST_CALL_IN_SEC = 5
+
 QUERY_URL_BASE = "https://open.er-api.com/v6/latest/{}"
 
 
@@ -50,6 +59,7 @@ def get_api_key(name: str):
     print(f"Generated key {k}")
     user = User(user_name=name, api_key=k)
     USER_COLLECTION.append(user)
+    API_KEY_TO_USER[k] = user
     VALID_APIS.add(k)
     return user
 
@@ -58,8 +68,16 @@ def get_api_key(name: str):
 def convert(api_key: str, curr_from: str, curr_to: str, amount: int = 1):
     if api_key not in VALID_APIS:
         return {"Error": "Your API KEY is invalid"}
+    user = API_KEY_TO_USER[api_key]
+    if user.credits <= 0:
+        return {"Error": "You do not have enough credits"}
+    now = datetime.utcnow()
+    if (now - API_KEY_TO_LAST_API_CALL[api_key]).total_seconds() < COOLDOWN_AFTER_LAST_CALL_IN_SEC:
+        return {"Error": "You are being rate-limited"}
 
     curr_to_amount = _convert(curr_from, curr_to, amount)
+    user.credits -= REQUEST_CREDIT_COST
+    API_KEY_TO_LAST_API_CALL[api_key] = now
     return {
         "curr_from": curr_from,
         "curr_from_amount": amount,
