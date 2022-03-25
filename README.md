4a0f8dcfc907b787c9b23392b220b6ff2858a28e
Gabor Meszaros <jegkocka88@gmail.com>
2022-03-25 14:44:48 +0100

    Functionally complete MVP + additional functionalities
    - Implemented:
        -> get_api_key
        -> list_currencies
        -> convert_currencies (real-time and historical)
    
    - Additional Feautures:
        -> check credits
        -> top-up credits

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# shake_challenge
+# shake_challenge - Currency Converter
+
+# Summary
+
+The Currency converter implementation offers currency exchange related information exposed
+on various endpoints. The users can register by requesting an api key, request historical as well as real-time exchange rates, manage their accounts (query/top-up credits) etc.
+The service is powered by https://currency.getgeoapi.com/
+
+# Endpoints
+
+- /supported_currencies: returns a list of 3-letter abbreviations of the currencies supported by the site. No API Key is needed for this functionality.
+
+- /get_api_key: registers a new user and provides an api key for querying.
+
+- /convert/: executes real-time conversion
+
+- /convert_historical/: executes historical conversion
+
+- /credits/: returns the credits available to the user.
+
+- /top_up_credits/: adds credits to the user's account
--- app.py
+++ app.py
@@ -1,36 +1,15 @@
 from collections import defaultdict
 from datetime import datetime
 from random import choices
+from string import ascii_lowercase, digits
 
 import requests
-from typing import Optional
 import uvicorn
 from fastapi import FastAPI
-
+from typing import Optional
 from pydantic import BaseModel
 
 from api_key import API_KEY
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
-class APIKeyRequest(BaseModel):
-    name: str
-
-
-class User(BaseModel):
-    user_name: str
-    api_key: str
-    credits: Optional[int] = 3
-
-
-def generate_random_api_key():
-    return ''.join(choices('abcdefg1234567890', k=12))
 
 
 app = FastAPI()
@@ -38,11 +17,12 @@ app = FastAPI()
 REQUEST_CREDIT_COST = 1
 
 USER_COLLECTION = []
-VALID_APIS = set()
+VALID_API_KEYS = set()
 API_KEY_TO_USER = {}
 
-API_KEY_TO_LAST_API_CALL = defaultdict(lambda: datetime.fromtimestamp(1))
-COOLDOWN_AFTER_LAST_CALL_IN_SEC = 3
+API_KEY_LENGTH = 12
+API_KEY_TO_LAST_API_CALL = defaultdict(list)
+QPM_LIMIT = 10
 
 # URLS
 SUPPORTED_CURRENCIES_BASE = "https://api.getgeoapi.com/v2/currency/list?api_key={api_key}"
@@ -52,14 +32,44 @@ HISTORICAL_CONVERT_URL_BASE =\
     "https://api.getgeoapi.com/v2/currency/historical/{year}-{month}-{day}?api_key={api_key}&from={from_}&to={to}&amount={amount}&format=json"
 
 
-def _convert(curr_from, curr_to, amount,):
-    url = CONVERT_URL_BASE.format(api_key=API_KEY, from_=curr_from, to=curr_to, amount=amount)
-    resp = requests.get(url).json()
-    unit_price = resp["rates"].get(curr_to)
-    return amount * unit_price if unit_price else None
+class User(BaseModel):
+    user_name: str
+    api_key: str
+    credits: Optional[int] = 3
 
 
-@app.get("/supported_currencies")
+def generate_random_api_key():
+    return ''.join(choices(ascii_lowercase + digits, k=API_KEY_LENGTH))
+
+def generate_unused_api_key():
+    for i in range(10 ** API_KEY_LENGTH):
+        k = generate_random_api_key()
+        if k not in VALID_API_KEYS:
+            VALID_API_KEYS.add(k)
+            return k
+
+
+def validate_request(api_key):
+    if api_key not in VALID_API_KEYS:
+        return False, {"Error": "Your API KEY is invalid"}
+    user = API_KEY_TO_USER[api_key]
+    if user.credits <= 0:
+        return False, {"Error": "You do not have enough credits; consider top-up!"}
+    now = datetime.utcnow().timestamp()
+    API_KEY_TO_LAST_API_CALL[api_key] = [t for t in API_KEY_TO_LAST_API_CALL[api_key] if now - t < 60]
+    if len(API_KEY_TO_LAST_API_CALL[api_key]) >= QPM_LIMIT:
+        return False, {"Error": "You are being rate-limited"}
+    return True, {}
+
+
+def update_user_stats(api_key):
+    user = API_KEY_TO_USER[api_key]
+    user.credits -= REQUEST_CREDIT_COST
+    API_KEY_TO_LAST_API_CALL[api_key].append(datetime.utcnow().timestamp())
+
+
+############################# ENDPOINTS #########################################
+@app.get("/supported_currencies/")
 def supported_currencies():
     """ 
     Returns the currencies supported by the 3rd party site.
@@ -72,32 +82,12 @@ def supported_currencies():
 @app.get("/get_api_key/")
 def get_api_key(name: str):
     print(f"{name} is requesting api")
-    k = generate_random_api_key()
+    k = generate_unused_api_key()
     print(f"Generated key {k}")
     user = User(user_name=name, api_key=k)
     USER_COLLECTION.append(user)
     API_KEY_TO_USER[k] = user
-    VALID_APIS.add(k)
     return user
-
-
-def validate_request(api_key):
-    if api_key not in VALID_APIS:
-        return False, {"Error": "Your API KEY is invalid"}
-    user = API_KEY_TO_USER[api_key]
-    if user.credits <= 0:
-        return False, {"Error": "You do not have enough credits"}
-    now = datetime.utcnow()
-    if (now - API_KEY_TO_LAST_API_CALL[api_key]).total_seconds() < COOLDOWN_AFTER_LAST_CALL_IN_SEC:
-        return False, {"Error": "You are being rate-limited"}
-    return True, {}
-
-
-def update_user_stats(api_key):
-    user = API_KEY_TO_USER[api_key]
-    user.credits -= REQUEST_CREDIT_COST
-    API_KEY_TO_LAST_API_CALL[api_key] = datetime.utcnow()
-    print(API_KEY_TO_LAST_API_CALL)
 
 
 @app.get("/convert/")
@@ -150,6 +140,40 @@ def convert_historical(api_key: str,
         "curr_to_amount": curr_to_amount,
         "date": f"{year}-{month}-{day}"
         }
+
+
+@app.get("/credits/")
+def get_credits(api_key: str):
+    """ Returns the available credits of the given key's user. """
+    if api_key not in VALID_API_KEYS:
+        return {"Error": "Your API KEY is invalid"}
+    user = API_KEY_TO_USER[api_key]
+    return {
+        "user": user.user_name,
+        "credits": user.credits
+    }
+
+
+def charge_bank_account(user, credit):
+    """ To be implemented """
+    pass  # :) This is where the money is at, but implementation is out of scope.
+
+
+@app.get("/top_up_credits/")
+def top_up_credits(api_key: str, credit: int = 1):
+    """ Add credits to a given user's account. """
+    if api_key not in VALID_API_KEYS:
+        return {"Error": "Your API KEY is invalid"}
+    if credit <= 0:
+        return {"Error": "Must apply positive credit"}
+    user = API_KEY_TO_USER[api_key]
+    charge_bank_account(user, credit)
+    user.credits += credit
+    return {
+        "Result": "successful top-up",
+        "user": user.user_name,
+        "new credits": user.credits
+    }
 
 
 if __name__ == "__main__":
